f3358875ce59513cc8a85d9b44cc175da6a985ef
nainireddy <nainivenkat@gmail.com>
2018-01-05 12:56:40 -0800

    Update sumoftwolargenumbers.py

--- sumoftwolargenumbers.py
+++ sumoftwolargenumbers.py
@@ -1,45 +1,33 @@
-#str1 = "7777555511111111"
-#str2 =  "3332222221111"
-str1 = "9"
-str2 =  "99"
+def sumoftwonumbers(str1,str2):
+    list1 = list(str1)
+    list2 = list(str2)
+    len1 = len(list1)
+    len2 = len(list2)
+    diff = abs(len1-len2)
+    if len1>len2:
+        for i in range(diff):
+            list2.insert(i,0)
+    else:
+        for i in range(diff):
+            list1.insert(i,0)
+    list1 = list1[::-1]
+    list2 = list2[::-1]
+    print list1
+    print list2
+    sum = 0
+    carry = 0
+    list3 = []
+    for i,j in zip(list1,list2):
+        
+        sum = i+j+carry
+        carry = sum //10
+        if sum >= 10:
+            sum = sum %10
 
+        list3.append(sum)
 
-list1 = list(str1)
-list2 = list(str2)
-print list1
-print list2
-if len(list1) > len(list2):
-    temp = list2
-    list2 = list1
-    list1 = temp
+    if carry:
+         list3.append(carry)
+    return list3[::-1]
 
-len1 = len(list1)
-len2 = len(list2)
-diff = len2 - len1
-
-for element in range(diff):
-    list1.insert(0,0)
-
-list3 = []
-
-list4 = list1[: : -1]
-list5 = list2[: : -1]
-
-len1 = len(list4)
-
-carry = 0
-list3 = []
-
-for index in range(len1):
-    sum = int(list4[index]) + int(list5[index]) + carry
-    carry = sum//10
-    if sum >=10:
-        sum = sum % 10
-    list3.append(sum)
-
-if carry:
-    list3.append(carry)
- 
-#print list3
-list3 = list3 [ : : -1]
-print list3
+print sumoftwonumbers("7777555511111111", "3332222221111")
