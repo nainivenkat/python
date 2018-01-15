38db91cb8f798109c370e594d375b924c2e1c377
nainireddy <nainivenkat@gmail.com>
2018-01-14 20:28:47 -0800

    Create string_compression.py

---
+++ excercises/string_compression.py
@@ -0,0 +1,9 @@
+string = "aabcccccaaa"
+count = 1
+string1 = ""
+for i in range(len(string)):
+    count +=1
+    if i+1 >= len(string) or string[i] != string[i+1]:
+        string1 += ""+string[i]+str(count)
+        count = 0
+print string1
