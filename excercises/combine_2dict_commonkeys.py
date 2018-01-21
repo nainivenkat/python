42dbe7645f7e4b75bd040af3a0bd08f3202edca6
nainireddy <nainivenkat@gmail.com>
2018-01-21 11:32:54 -0800

    Update combine_2dict_commonkeys.py

--- excercises/combine_2dict_commonkeys.py
+++ excercises/combine_2dict_commonkeys.py
@@ -3,15 +3,14 @@ d1 = {'a': 100, 'b': 200, 'c':300}
 d2 = {'a': 300, 'b': 200, 'd':400}
 
 d3 = {}
-for key, value in d1.items():
-    for key1,value1 in d2.items():
-        if key == key1:
-            d3[key] = value + value1
-        elif key not in d3.keys():
-            d3[key] = value
-        elif key1 not in d3.keys():
+for key1, value1 in d1.items():
+    for key2, value2 in d2.items():
+        if key1 == key2:
+            d3[key1] = value1+ value2
+        elif key1 not in d3.keys() and key1 not in d2.keys():
             d3[key1] = value1
-
+        elif key2 not in d3.keys() and key2 not in d1.keys():
+            d3[key2] = value2
 print d3
 
 d4 = {}
