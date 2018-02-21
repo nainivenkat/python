83efc983e32930ea7b3469c92ec6b5439f156313
nainireddy <nainivenkat@gmail.com>
2018-02-20 16:00:28 -0800

    Create list_modification.py

---
+++ excercises/list_modification.py
@@ -0,0 +1,9 @@
+#To get every thrid element in the list:
+x= [1, 2, 3, 4, 5, 6, 7, 8, 9]
+x = x[::3]
+print x
+
+# To remove every thrid element in the list
+list1 = [1,2,3,4,5,6,7,8,9]
+del list1[0::3]
+print list1
