b311e186eb32ca0729422bd8fdf3ef9fe22881fd
nainireddy <nainivenkat@gmail.com>
2018-04-05 06:12:47 -0700

    Update string_compression.py

--- excercises/string_compression.py
+++ excercises/string_compression.py
@@ -1,9 +1,23 @@
-string = "aabcccccaaa"
-count = 0
-string1 = ""
-for i in range(len(string)):
-    count +=1
-    if i+1 >= len(string) or string[i] != string[i+1]:
-        string1 += ""+string[i]+str(count)
-        count = 0
-print string1
+def compress(string):
+    len1 = len(string)
+    i = 1
+    count = 1
+    r = ""
+
+    if len1 == 0:
+        return " "
+    if len1 == 1:
+        return string+'1'
+    
+    while i < len1:
+        if string[i] == string[i-1]:
+            count +=1
+        else:
+            r = r+string[i-1]+str(count)
+            count = 1
+        i = i+1
+    r = r+string[i-1]+str(count)
+    return r
+
+string = "aaabbccccab"
+print compress(string)
