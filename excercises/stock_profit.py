a38355067aa3d15c39caa9670587d3e5a1e46f70
nainireddy <nainivenkat@gmail.com>
2018-05-01 17:02:19 -0700

    Update stock_profit.py

--- excercises/stock_profit.py
+++ excercises/stock_profit.py
@@ -1,14 +1,23 @@
 def profit(arr):
-    if len(arr) == 1:
-        return arr
+    # Check length
+    if len(stock_prices) < 2:
+        raise Exception('Need at least two stock prices!')
+    
+    # Start minimum price marker at first price
+    min_stock_price = stock_prices[0]
+    
+    # Start off with an initial max profit
+    max_profit = stock_prices[1] - stock_prices[0]
+    
+    # Skip first index of 0
+    for price in stock_prices[1:]:
 
-    min_price = arr[0]
-    max_profit = 0
+        comparison_profit = price - min_stock_price
 
-    for value in arr[1:]:
-        min_price = min(min_price,value)
-        comp_profit = value - min_price
-        max_profit = max(max_profit,comp_profit)
+        max_profit = max(max_profit,comparison_profit)
+
+        min_stock_price = min(min_stock_price,price)
+        
     return max_profit
 
 prices = [12,3,11,15,10]
